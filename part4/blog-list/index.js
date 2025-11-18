d6cb544500b3e74ac46da542aa91bc0342f4240f
jadera <jadera@proton.me>
2025-11-18 19:56:32 +0200

    bloglist step 1 done

---
+++ part4/blog-list/index.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+const blogSchema = mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number,
+})
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = 'mongodb+srv://poopstack:nukemypants@cluster0.ndc3ai3.mongodb.net/?appName=Cluster0'
+mongoose.connect(mongoUrl, { family: 4 })
+
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+    Blog.find({}).then((blogs) => {
+        response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+    const blog = new Blog(request.body)
+
+    blog.save().then((result) => {
+        response.status(201).json(result)
+    })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
